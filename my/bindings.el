bd03154d0c9ccbe008778151a5c22d3a7aa1b22a
poutyface <tom.enpuku@gmail.com>
2011-05-07 10:14:19 +0900

    add moving window binding

--- my/bindings.el
+++ my/bindings.el
@@ -5,3 +5,11 @@
 (defun iswitchb-my-key ()
   (define-key iswitchb-mode-map [right] 'iswitchb-next-match)
   (define-key iswitchb-mode-map [left] 'iswitchb-prev-match))
+
+; window moving 
+(setq windmove-wrap-around t)
+(define-key global-map [C-up] 'windmove-up)
+(define-key global-map [C-down] 'windmove-down)
+(define-key global-map [C-right] 'windmove-right)
+(define-key global-map [C-left] 'windmove-left)
+;(windmove-default-keybindings)
